5148446b5d1bdb79d1c8286ad98ef1f442c3dcac
gayathri499 <srigayathrimurugan.mani@gmail.com>
2026-07-06 12:07:15 +0530

    Added Bootstrap dashboard

--- utils/ats_score.py
+++ utils/ats_score.py
@@ -1,6 +1,9 @@
+from utils.skill_extractor import ALL_SKILLS
+
+
 def calculate_ats_score(found_skills):
 
-    total_skills = 21
+    total_skills = len(ALL_SKILLS)
 
     score = int((len(found_skills) / total_skills) * 100)
 
--- utils/skill_extractor.py
+++ utils/skill_extractor.py
@@ -1,38 +1,42 @@
-def extract_skills(text):
+ALL_SKILLS = [
+    "Python",
+    "Java",
+    "C",
+    "C++",
+    "SQL",
+    "HTML",
+    "CSS",
+    "JavaScript",
+    "Flask",
+    "Django",
+    "Machine Learning",
+    "Deep Learning",
+    "Power BI",
+    "Tableau",
+    "Git",
+    "GitHub",
+    "Excel",
+    "Pandas",
+    "NumPy",
+    "TensorFlow",
+    "Scikit-learn",
+    "Docker",
+    "AWS",
+    "React"
+]
 
-    all_skills = [
-        "Python",
-        "Java",
-        "C",
-        "C++",
-        "SQL",
-        "HTML",
-        "CSS",
-        "JavaScript",
-        "Flask",
-        "Django",
-        "Machine Learning",
-        "Deep Learning",
-        "Power BI",
-        "Tableau",
-        "Git",
-        "GitHub",
-        "Excel",
-        "Pandas",
-        "NumPy",
-        "TensorFlow",
-        "Scikit-learn"
-    ]
+
+def extract_skills(text):
 
     found_skills = []
 
-    for skill in all_skills:
+    for skill in ALL_SKILLS:
         if skill.lower() in text.lower():
             found_skills.append(skill)
 
     missing_skills = []
 
-    for skill in all_skills:
+    for skill in ALL_SKILLS:
         if skill not in found_skills:
             missing_skills.append(skill)
 
